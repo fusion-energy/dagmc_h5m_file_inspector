9dfb259b69dffe7fa65bb5b8920554ed4aeb0a83
autopep8 <autopep8@users.noreply.github.com>
2021-09-13 15:06:28 +0000

    Automated autopep8 fixes

--- tests/test_command_line_usage.py
+++ tests/test_command_line_usage.py
@@ -5,35 +5,68 @@ from pathlib import Path
 import subprocess
 import json
 
-class TestReactor(unittest.TestCase):
 
+class TestReactor(unittest.TestCase):
 
     def test_volume_finding(self):
         """Tests command runs and produces an output files with the correct contents"""
         os.system('rm vols.json')
-        os.system('inspect-dagmc-h5m-file -i tests/fusion_example_for_openmc_using_paramak-0.0.1/dagmc.h5m -v -o vols.json')
+        os.system(
+            'inspect-dagmc-h5m-file -i tests/fusion_example_for_openmc_using_paramak-0.0.1/dagmc.h5m -v -o vols.json')
         assert Path('vols.json').is_file()
         with open('vols.json') as jsonFile:
             jsonObject = json.load(jsonFile)
-        assert jsonObject == {'volumes': [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22]}
+        assert jsonObject == {
+            'volumes': [
+                1,
+                2,
+                3,
+                4,
+                5,
+                6,
+                7,
+                8,
+                9,
+                10,
+                11,
+                12,
+                13,
+                14,
+                15,
+                16,
+                17,
+                18,
+                19,
+                20,
+                21,
+                22]}
 
     def test_material_finding(self):
         """Tests command runs and produces an output files with the correct contents"""
         os.system('rm vols.json')
-        os.system('inspect-dagmc-h5m-file -i tests/fusion_example_for_openmc_using_paramak-0.0.1/dagmc.h5m -m -o mats.json')
+        os.system(
+            'inspect-dagmc-h5m-file -i tests/fusion_example_for_openmc_using_paramak-0.0.1/dagmc.h5m -m -o mats.json')
         assert Path('mats.json').is_file()
         with open('mats.json') as jsonFile:
             jsonObject = json.load(jsonFile)
-        assert jsonObject == {'materials':[ "Vacuum", "copper", "flibe", "graveyard", "steel", "tungsten"]}
+        assert jsonObject == {
+            'materials': [
+                "Vacuum",
+                "copper",
+                "flibe",
+                "graveyard",
+                "steel",
+                "tungsten"]}
 
     def test_both_finding(self):
         """Tests command runs and produces an output files with the correct contents"""
         os.system('rm vols.json')
-        os.system('inspect-dagmc-h5m-file -i tests/fusion_example_for_openmc_using_paramak-0.0.1/dagmc.h5m -b -o both.json')
+        os.system(
+            'inspect-dagmc-h5m-file -i tests/fusion_example_for_openmc_using_paramak-0.0.1/dagmc.h5m -b -o both.json')
         assert Path('both.json').is_file()
         with open('both.json') as jsonFile:
             jsonObject = json.load(jsonFile)
-        assert jsonObject == {'both':{
+        assert jsonObject == {'both': {
             '1': "tungsten",
             '2': "steel",
             '3': "steel",
@@ -61,7 +94,8 @@ class TestReactor(unittest.TestCase):
     def test_no_user_output(self):
         """Tests command runs and produces an output files with the correct contents"""
         os.system('rm empty.json')
-        os.system('inspect-dagmc-h5m-file -i tests/fusion_example_for_openmc_using_paramak-0.0.1/dagmc.h5m -o empty.json')
+        os.system(
+            'inspect-dagmc-h5m-file -i tests/fusion_example_for_openmc_using_paramak-0.0.1/dagmc.h5m -o empty.json')
         assert Path('empty.json').is_file() == False
 
 # todo improve tests to check commands run correctly
@@ -71,7 +105,6 @@ class TestReactor(unittest.TestCase):
 
     def test_no_user_input(self):
         os.system('inspect-dagmc-h5m-file')
-
 
 
 if __name__ == "__main__":
